b7fd310dcb18aefc6db9502823e8dfd6b3b8b940
tkmsaaaam <59013192+tkmsaaaam@users.noreply.github.com>
2022-10-29 10:57:58 +0900

    add then()

--- ec-finder/src/popup.js
+++ ec-finder/src/popup.js
@@ -4,22 +4,22 @@
 	document.getElementsByTagName('button')[0].addEventListener('click', e => {
 		e.preventDefault();
 		const word = document.getElementsByTagName('input')[0].value;
-		chrome.tabs.create({ url: RAKUTEN + word });
-		chrome.tabs.create({ url: AMAZON + word });
+		chrome.tabs.create({ url: RAKUTEN + word }).then();
+		chrome.tabs.create({ url: AMAZON + word }).then();
 	});
-	document.getElementsByTagName('button')[1].addEventListener('click', e => {
+	document.getElementsByTagName('button')[1].addEventListener('click', () => {
 		chrome.tabs.query({ active: true, lastFocusedWindow: true }, tabs => {
 			chrome.tabs.sendMessage(tabs[0].id, { message: 'getTitle' }, response => {
 				const word = response.replaceAll(' ' || '　', '+');
-				chrome.tabs.create({ url: AMAZON + word });
+				chrome.tabs.create({ url: AMAZON + word }).then();
 			});
 		});
 	});
-	document.getElementsByTagName('button')[2].addEventListener('click', e => {
+	document.getElementsByTagName('button')[2].addEventListener('click', () => {
 		chrome.tabs.query({ active: true, lastFocusedWindow: true }, tabs => {
 			chrome.tabs.sendMessage(tabs[0].id, { message: 'getTitle' }, response => {
 				const word = response.replaceAll(' ' || '　', '+');
-				chrome.tabs.create({ url: RAKUTEN + word });
+				chrome.tabs.create({ url: RAKUTEN + word }).then();
 			});
 		});
 	});
