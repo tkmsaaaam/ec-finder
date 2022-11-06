5eb0950cb176ec794e7bbb55c6db7eaa9f753951
tkmsaaaam <59013192+tkmsaaaam@users.noreply.github.com>
2022-11-07 08:33:35 +0900

    refactoring

--- ec-finder/src/popup.ts
+++ ec-finder/src/popup.ts
@@ -1,62 +1,64 @@
-const ALL = 'ALL';
-const RAKUTEN = 'RAKUTEN';
-const AMAZON = 'AMAZON';
+const RAKUTEN: string = 'RAKUTEN';
+const AMAZON: string = 'AMAZON';
+
+const SEARCH_BUTTON: HTMLButtonElement =
+	document.getElementsByTagName('button')[0];
+const AMAZON_BUTTON: HTMLButtonElement =
+	document.getElementsByTagName('button')[1];
+const RAKUTEN_BUTTON: HTMLButtonElement =
+	document.getElementsByTagName('button')[2];
+
+const RAKUTEN_URL: string = 'https://search.rakuten.co.jp/search/mall/';
+const AMAZON_URL: string = 'https://www.amazon.co.jp/s?k=';
+
+type SiteList = Site[];
+type Site = {
+	name: string;
+	html: HTMLButtonElement;
+	url: string;
+};
+const siteList: SiteList = [
+	{
+		name: AMAZON,
+		html: AMAZON_BUTTON,
+		url: AMAZON_URL,
+	},
+	{
+		name: RAKUTEN,
+		html: RAKUTEN_BUTTON,
+		url: RAKUTEN_URL,
+	},
+];
 
 const makeQueryParam = (word: string): string => {
 	return word.replaceAll(' ' || '　', '+');
 };
 
-const createTabs = (word: string, site: string): void => {
-	if (site === RAKUTEN || site === ALL) {
-		chrome.tabs
-			.create({ url: 'https://search.rakuten.co.jp/search/mall/' + word })
-			.then();
-	}
-	if (site === AMAZON || site === ALL) {
-		chrome.tabs.create({ url: 'https://www.amazon.co.jp/s?k=' + word }).then();
-	}
-};
-
 ((): void => {
-	(
-		document.getElementsByTagName('button')[0] as HTMLButtonElement
-	).addEventListener('click', (e: MouseEvent): void => {
+	SEARCH_BUTTON.addEventListener('click', (e: MouseEvent): void => {
 		e.preventDefault();
 		const word: string = makeQueryParam(
 			(document.getElementsByTagName('input')[0] as HTMLInputElement).value
 		);
-		createTabs(word, ALL);
+		chrome.tabs.create({ url: AMAZON_URL + word }).then();
+		chrome.tabs.create({ url: RAKUTEN_URL + word }).then();
 	});
-	(
-		document.getElementsByTagName('button')[1] as HTMLButtonElement
-	).addEventListener('click', (): void => {
-		chrome.tabs.query(
-			{ active: true, lastFocusedWindow: true },
-			(tabs: chrome.tabs.Tab[]): void => {
-				chrome.tabs.sendMessage(
-					tabs[0].id as number,
-					{ message: 'getTitle' },
-					(response): void => {
-						createTabs(makeQueryParam(response), AMAZON);
-					}
-				);
-			}
-		);
-	});
-	(
-		document.getElementsByTagName('button')[2] as HTMLButtonElement
-	).addEventListener('click', (): void => {
-		chrome.tabs.query(
-			{ active: true, lastFocusedWindow: true },
-			(tabs: chrome.tabs.Tab[]) => {
-				chrome.tabs.sendMessage(
-					tabs[0].id as number,
-					{ message: 'getTitle' },
-					(response): void => {
-						createTabs(makeQueryParam(response), RAKUTEN);
-					}
-				);
-			}
-		);
-	});
+	for (let site of siteList) {
+		site.html.addEventListener('click', (): void => {
+			chrome.tabs.query(
+				{ active: true, lastFocusedWindow: true },
+				(tabs: chrome.tabs.Tab[]): void => {
+					chrome.tabs.sendMessage(
+						tabs[0].id as number,
+						{ message: 'getTitle' },
+						(response): void => {
+							chrome.tabs
+								.create({ url: site.url + makeQueryParam(response) })
+								.then();
+						}
+					);
+				}
+			);
+		});
+	}
 })();
